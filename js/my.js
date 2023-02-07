e5f0b2d44cf655f211010bc426fad53a007d68bf
“markantonio99” <markomakkia@gmail.com>
2023-02-07 15:54:28 +0100

    push

---
+++ js/my.js
@@ -0,0 +1,21 @@
+
+
+// let numArr = []
+// console.log(numArr)
+
+
+
+while (nUm === dispari){
+
+    let numInserito = prompt(parseInt('inserisci un numero'))
+    console.log(numInserito)
+
+
+    numArr.push(numInserito)
+
+
+    let dispari = numInserito % 1
+    console.log(dispari)
+
+}
+
